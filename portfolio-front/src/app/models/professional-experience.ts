2868aa85cc3ccbf4f9474d7481e4de8b0314e470
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-08-26 09:36:36 -0300

    Add language, professional-experience, professional-network & user-information models

---
+++ portfolio-front/src/app/models/professional-experience.ts
@@ -0,0 +1,8 @@
+export interface ProfessionalExperience {
+    company: string;
+    position: string;
+    startDate: Date;
+    endDate: Date | null;
+    description: string;
+    stack: string[];
+}
---
+++ portfolio-front/src/app/models/user-information.ts
@@ -0,0 +1,16 @@
+import { Language } from "./language";
+import { ProfessionalExperience } from "./professional-experience";
+import { ProfessionalNetwork } from "./professional-network";
+
+export interface UserInformation {
+    name: string;
+    nickname: string;
+    degree: string;
+    about: string;
+    location: string;
+    workAuthorization: string;
+    email: string;
+    languages: Language[];
+    professionalExperiences: ProfessionalExperience[];
+    professionalNetworks: ProfessionalNetwork[];
+}

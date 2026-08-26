c8ab269d1b3bdad4ea76735f4cafa28694429a6f
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-08-26 09:39:03 -0300

    Generate professional-experience component

--- portfolio-front/src/app/professional-experience/professional-experience.ts
+++ portfolio-front/src/app/professional-experience/professional-experience.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserInformationService } from '../services/user-information-service';
 
 @Component({
   selector: 'app-professional-experience',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './professional-experience.html',
   styleUrl: './professional-experience.scss',
 })
-export class ProfessionalExperience {}
+export class ProfessionalExperience {
+  private readonly userInformationService = inject(UserInformationService);
+
+  readonly professionalExperiences = this.userInformationService.getProfessionalExperiences();
+}

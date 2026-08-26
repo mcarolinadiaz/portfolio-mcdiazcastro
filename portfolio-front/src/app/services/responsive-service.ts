584279e6aa467d39ee1a1d204c30d0c3cf7d8257
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-08-26 09:34:49 -0300

    Add responsive service to centralize breakpoints conditions

---
+++ portfolio-front/src/app/services/responsive-service.ts
@@ -0,0 +1,29 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { inject, Service } from '@angular/core';
+import { BOOTSTRAP_BREAKPOINTS } from '../core/constants/breakpoints';
+import { map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Service()
+export class ResponsiveService {
+    private breakpointObserver = inject(BreakpointObserver);
+
+    readonly isMobile = this.createSignal(BOOTSTRAP_BREAKPOINTS.isMobile);
+    readonly isTablet = this.createSignal(BOOTSTRAP_BREAKPOINTS.isTablet);
+    readonly isDesktop = this.createSignal(BOOTSTRAP_BREAKPOINTS.isDesktop);
+
+    readonly isExtraSmall = this.createSignal(BOOTSTRAP_BREAKPOINTS.xs);
+    readonly isSmall = this.createSignal(BOOTSTRAP_BREAKPOINTS.sm);
+    readonly isMedium = this.createSignal(BOOTSTRAP_BREAKPOINTS.md);
+    readonly isLargeScreen = this.createSignal(BOOTSTRAP_BREAKPOINTS.lg);
+    readonly isExtraLargeScreen = this.createSignal(BOOTSTRAP_BREAKPOINTS.xl);
+
+    private createSignal(query: string) {
+        const stream$ = this.breakpointObserver
+        .observe([query])
+        .pipe(map(result => result.matches));
+
+        return toSignal(stream$, { initialValue: false });
+    }
+}
+

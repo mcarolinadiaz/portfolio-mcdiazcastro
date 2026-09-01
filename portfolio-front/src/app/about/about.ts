adeaff5294d713d02d3ef1d36d4730bc7db8c771
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-09-01 11:59:20 -0300

    u - upd about component - add styles & using center-content component as a child

--- portfolio-front/src/app/about/about.ts
+++ portfolio-front/src/app/about/about.ts
@@ -1,14 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { UserInformationService } from '../services/user-information-service';
+import { ResponsiveService } from '../services/responsive-service';
+import { CenterContent } from "../shared/center-content/center-content";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CenterContent],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
 export class About {
   private readonly userInformationService = inject(UserInformationService);
+  protected responsiveService = inject(ResponsiveService);
+  
 
   readonly user = this.userInformationService.user();
 

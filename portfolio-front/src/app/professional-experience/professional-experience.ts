39dadb96ba600791cb3b9f1f76b8deaa1c8ae345
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-09-01 11:52:01 -0300

    u - update professional experience component - it uses card-information component as a child component

--- portfolio-front/src/app/professional-experience/professional-experience.ts
+++ portfolio-front/src/app/professional-experience/professional-experience.ts
@@ -1,14 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { UserInformationService } from '../services/user-information-service';
+import { CardInformation } from "./card-information/card-information";
 
 @Component({
   selector: 'app-professional-experience',
-  imports: [],
+  imports: [CardInformation],
   templateUrl: './professional-experience.html',
   styleUrl: './professional-experience.scss',
 })
 export class ProfessionalExperience {
   private readonly userInformationService = inject(UserInformationService);
 
-  readonly professionalExperiences = this.userInformationService.getProfessionalExperiences();
+  professionalExperiences = this.userInformationService.getProfessionalExperiences();
 }

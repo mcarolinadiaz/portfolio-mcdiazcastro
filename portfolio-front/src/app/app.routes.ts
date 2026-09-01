9ee20d77d64282dcf560b28fec8d00026ab985c9
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-09-01 11:42:18 -0300

    d - delete technical skills component

--- portfolio-front/src/app/app.routes.ts
+++ portfolio-front/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { ProfessionalExperience } from './professional-experience/professional-experience';
-import { TechnicalSkills } from './technical-skills/technical-skills';
 import { Education } from './education/education';
 import { Contact } from './contact/contact';
 import { About } from './about/about';
@@ -13,12 +12,10 @@ export const routes: Routes = [
       path: '', 
       component: Home,
       children: [
-        { path: '', redirectTo: 'about', pathMatch: 'full' }, // default route
-        { path: 'about', component: About, title: 'About me | MCDíazCastro' },
-        { path: 'professional-experience', component: ProfessionalExperience, title: 'Professional Experience | MCDíazCastro' },
-        { path: 'technical-skills', component: TechnicalSkills, title: 'Technical Skills | MCDíazCastro' },
-        { path: 'education', component: Education, title: 'Education | MCDíazCastro' },
-        { path: 'keep-in-touch', component: Contact, title: 'Keep in Touch | MCDíazCastro' }
+        { path: '', component: About, title: 'Carolina Díaz | About me' },
+        { path: 'professional-experience', component: ProfessionalExperience, title: 'Carolina Díaz | Professional Experience' },
+        { path: 'education', component: Education, title: 'Carolina Díaz | Education' },
+        { path: 'keep-in-touch', component: Contact, title: 'Carolina Díaz | Keep in Touch' }
       ]
     },
     { path: '**', redirectTo: '', pathMatch: 'full' }
--- portfolio-front/src/app/technical-skills/technical-skills.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-technical-skills',
-  imports: [],
-  templateUrl: './technical-skills.html',
-  styleUrl: './technical-skills.scss',
-})
-export class TechnicalSkills {}

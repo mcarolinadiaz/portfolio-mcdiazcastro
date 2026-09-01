b20312ea180c15c5aee837c938e80643bb31e9cd
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-09-01 11:54:36 -0300

    fix - fix when item is active & update dropdown-menu component styles

--- portfolio-front/src/app/navbar/dropdown-menu/dropdown-menu.ts
+++ portfolio-front/src/app/navbar/dropdown-menu/dropdown-menu.ts
@@ -1,26 +1,26 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { Route, Router, RouterLink } from '@angular/router';
+import { Route, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ResponsiveService } from '../../services/responsive-service';
 
 @Component({
   selector: 'app-dropdown-menu',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './dropdown-menu.html',
   styleUrl: './dropdown-menu.scss',
 })
-export class DropdownMenu implements OnInit {
+export class DropdownMenu {
   private router = inject(Router);
   protected responsiveService = inject(ResponsiveService);
   
   appRoutes: Route[] = [];
 
-  ngOnInit(): void {
+  constructor() {
     this.appRoutes = this.router.config.at(0)?.children?.filter((child: any) => child.path !== undefined && child.title !== undefined)
     .map((child: any) => 
     {
       return {
         path: child.path,
-        title: String(child.title)?.split('|')[0] ?? '', 
+        title: String(child.title)?.split('|')[1] ?? '', 
       };
     }) ?? [];
   }

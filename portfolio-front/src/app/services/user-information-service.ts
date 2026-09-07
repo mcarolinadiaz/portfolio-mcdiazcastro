90e3e79010c51b7755597dd9f3937107dfdb8498
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-09-07 09:21:31 -0300

    u - update description companies

--- portfolio-front/src/app/services/user-information-service.ts
+++ portfolio-front/src/app/services/user-information-service.ts
@@ -29,7 +29,9 @@ export class UserInformationService {
             position: "Software Developer",
             startDate: new Date("2024-09-01"),
             endDate: new Date("2026-05-31"),
-            description: "Front-End Manager leading development initiatives. Developed web applications using Angular (HTML, SCSS, TypeScript) and .NET (C#). Worked with Odoo, Magento 2, PHP, JavaScript, and LESS. Delivered scalable and maintainable solutions following best practices.",
+            description: `Front-End Manager leading development initiatives. 
+                Developed web applications using Angular (HTML, SCSS, TypeScript) and .NET (C#). 
+                Worked with Odoo, Magento 2, PHP, JavaScript, and LESS. Delivered scalable and maintainable solutions following best practices.`,
             stack: ["Angular-v18", "TypeScript", "SCSS", ".NET", "C#", "Odoo", "Magento 2", "PHP", "JavaScript", "LESS"]
         },
         {
@@ -37,7 +39,8 @@ export class UserInformationService {
             position: "Software Developer",
             startDate: new Date("2024-01-01"),
             endDate: new Date("2024-09-30"),
-            description: "Developed and maintained applications using Angular. Implemented scalable and maintainable software solutions.",
+            description: `Developed and maintained applications using Angular. 
+                Implemented scalable and maintainable software solutions.`,
             stack: ["Angular-v18", "TypeScript", "SCSS", "JavaScript"]
         },
         {
@@ -45,7 +48,8 @@ export class UserInformationService {
             position: "Front-End Developer",
             startDate: new Date("2022-03-01"),
             endDate: new Date("2023-01-31"),
-            description: "Developed and maintained web applications using Angular and WordPress. Provided support and maintenance for existing systems.",
+            description: `Developed and maintained web applications using Angular and WordPress. 
+                Provided support and maintenance for existing systems.`,
             stack: ["Angular-v15", "WordPress", "PHP", "JavaScript", "SCSS"]
         },
         {
@@ -53,7 +57,9 @@ export class UserInformationService {
             position: "Front-End Developer",
             startDate: new Date("2022-10-01"),
             endDate: new Date("2023-01-31"),
-            description: "Developed and maintained frontend web applications. Implemented scalable solutions in cloud-based environments. Applied best practices in software development.",
+            description: `Developed and maintained frontend web applications. 
+                Implemented scalable solutions in cloud-based environments. 
+                Applied best practices in software development.`,
             stack: ["Angular-v15", "WordPress", "PHP", "JavaScript", "SCSS"]
         },
         {
@@ -61,7 +67,9 @@ export class UserInformationService {
             position: "Software Developer",
             startDate: new Date("2021-05-01"),
             endDate: new Date("2021-12-31"),
-            description: "Worked on support and maintenance using PL/SQL, JavaScript, and JSP. Developed frontend features using Angular. Used tools such as Azure.",
+            description: `Worked on support and maintenance using PL/SQL, JavaScript, and JSP. 
+                Developed frontend features using Angular. 
+                Used tools such as Azure.`,
             stack: ["PL/SQL", "JavaScript", "JSP", "Angular-v15", "Azure"]
         },
         {
@@ -69,7 +77,10 @@ export class UserInformationService {
             position: "Front-End Developer",
             startDate: new Date("2020-08-01"),
             endDate: new Date("2021-05-31"),
-            description: "Developed user interfaces using Angular (HTML, SCSS, TypeScript). Improved usability and performance of applications. Worked with JSON data and version control tools (Git, Taiga). Participated in UX/UI design.",
+            description: `Developed user interfaces using Angular (HTML, SCSS, TypeScript). 
+                Improved usability and performance of applications. 
+                Worked with JSON data and version control tools (Git, Taiga). 
+                Participated in UX/UI design.`,
             stack: ["Angular-v11", "HTML", "SCSS", "TypeScript"]
         }
     ];
@@ -97,7 +108,10 @@ export class UserInformationService {
         name: 'Carolina',
         nickname: 'Caro',
         degree: 'Software engineer',
-        about: 'I am a software engineer...',
+        about: `I'm a software engineer who is constantly seeking to learn and grow in the field of software development. I've expertise in front-end development, particularly with Angular, and I'm always eager to explore new technologies and frameworks.
+        I have a strong foundation in programming and problem-solving, and I'm passionate about writing clean, efficient code. I'm also interested in back-end development and cloud technologies.
+        Currently, I teach at the university as a Teaching Assistant, I enjoy sharing my knowledge with students. Moreover, I'm always looking for new challengues and opportunities.
+        In my spare time, I love dancing Tango, doing pottery and spending time with my partner and my friends. As for sports, I like doing hiking in the hills and doing water sports.`,
         location: 'Tandil, Buenos Aires, Argentina',
         workAuthorization: 'EU / Argentina',
         email: 'diazmariacarolina6@gmail.com',

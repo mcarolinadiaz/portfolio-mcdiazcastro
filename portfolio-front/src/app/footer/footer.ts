ace3a99caeced0cf2f24f8d45358508f3c4b47aa
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-09-16 14:58:35 -0300

    [UPD] - update footer to show safe url

--- portfolio-front/src/app/footer/footer.ts
+++ portfolio-front/src/app/footer/footer.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { UserInformationService } from '../shared/services/user-information-service';
+import { UrlSanitizerService } from '../shared/services/url-sanitizer-service';
 
 @Component({
   selector: 'app-footer',
@@ -9,6 +10,12 @@ import { UserInformationService } from '../shared/services/user-information-serv
 })
 export class Footer {
   private readonly userInformationService = inject(UserInformationService);
-
+  private readonly urlSanitizerService = inject(UrlSanitizerService);
   readonly professionalNetworks = this.userInformationService.getProfessionalNetworks();
+  readonly angularSite = this.urlSanitizerService.angularSite;
+  readonly bootstrapSite = this.urlSanitizerService.bootstrapSite;
+  readonly vscSite = this.urlSanitizerService.vscSite;
+  readonly vitestSite = this.urlSanitizerService.vitestSite;
+  readonly vercelSite = this.urlSanitizerService.vercelSite;
+  readonly copilotSite = this.urlSanitizerService.copilotSite;
 }

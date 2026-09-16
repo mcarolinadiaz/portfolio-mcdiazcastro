e752023b5b314182afca0be014f8cd132c5f2914
mcarolinadiaz <diazmariacarolina6@gmail.com>
2026-09-16 14:59:50 -0300

    [ADD] - update education section and call components childs

--- portfolio-front/src/app/education/education.ts
+++ portfolio-front/src/app/education/education.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserInformationService } from '../shared/services/user-information-service';
+import { AccordionComponent } from './accordion-component/accordion-component';
+import { EducationItem } from './education-item/education-item';
+import { ListGroup } from '../shared/list-group/list-group';
 
 @Component({
   selector: 'app-education',
-  imports: [],
+  imports: [AccordionComponent, EducationItem, ListGroup],
   templateUrl: './education.html',
   styleUrl: './education.scss',
 })
-export class Education {}
+export class Education {
+  private readonly userInformationService = inject(UserInformationService);
+  readonly technicalSkills = this.userInformationService.getTechnicalSkills();
+  readonly education = this.userInformationService.getEducation();
+  readonly certifications = this.userInformationService.getCertifications();
+  readonly languages = this.userInformationService.getLanguages();
+}
